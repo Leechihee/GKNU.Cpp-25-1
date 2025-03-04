c30f487dc74013305aab0a9dc69c11a292053664
Leechihee <119794981+Leechihee@users.noreply.github.com>
2025-03-04 11:38:48 +0900

    Create 250304-2.cpp

---
+++ 250304-2.cpp
@@ -0,0 +1,15 @@
+// 두 수를 함수에서 더하고 출력하시오.
+#include <iostream>
+using namespace std;
+
+int add(int a, int b)
+{
+  return a+b;
+}
+
+int main()
+{
+  int a = 2, b = 2;
+  cout << add(a,b);
+  return 0;
+}
